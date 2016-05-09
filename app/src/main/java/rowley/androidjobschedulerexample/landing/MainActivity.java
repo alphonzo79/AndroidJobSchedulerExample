73dcab016bfe272158a58ed1a0d8ff2a2b133305
Joe Rowley (jrowley) <joseph.rowley@mlb.com>
2016-05-09 17:12:43 -0600

    Setting up the JobScheduler example

--- app/src/main/java/rowley/androidjobschedulerexample/landing/MainActivity.java
+++ app/src/main/java/rowley/androidjobschedulerexample/landing/MainActivity.java
@@ -1,8 +1,13 @@
 package rowley.androidjobschedulerexample.landing;
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -12,6 +17,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rowley.androidjobschedulerexample.R;
 import rowley.androidjobschedulerexample.scheduled.alarm.ServiceFromAlarm;
+import rowley.androidjobschedulerexample.scheduled.scheduler.DataSyncJobService;
 import rowley.androidjobschedulerexample.scheduled.simple.ServiceWithHandler;
 
 /**
@@ -20,6 +26,8 @@ import rowley.androidjobschedulerexample.scheduled.simple.ServiceWithHandler;
  */
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
+
+    private final int JOB_ID = 1;
 
     @Bind(R.id.label)
     TextView label;
@@ -43,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.alarmButton)
     void onAlarmButtonClick() {
+        setupAlarmService();
+    }
+
+    @OnClick(R.id.jobSchedulerButton)
+    void onJobSchedulerButtonClick() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setupJobScheduler();
+        } else {
+            setupAlarmService();
+        }
+    }
+
+    private void setupAlarmService() {
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         PendingIntent pendingIntent =
                 PendingIntent.getService(this, 0, new Intent(this, ServiceFromAlarm.class), PendingIntent.FLAG_CANCEL_CURRENT);
@@ -53,8 +74,62 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    @OnClick(R.id.jobSchedulerButton)
-    void onJobSchedulerButtonClick() {
-        // TODO: 5/9/16
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setupJobScheduler() {
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancel(JOB_ID);
+
+        //Some housekeeping to reset things. The choice to use SharedPreferences is discussed in DataSyncJobService.
+        //I don't think it's the best choice, but it's a choice that works.
+        getSharedPreferences(
+                DataSyncJobService.SHARED_PREFS_KEY, MODE_PRIVATE).edit().
+                putInt(DataSyncJobService.FILES_COMPLETED_BUNDLE_KEY, 0).apply();
+
+        JobInfo.Builder jobBuilder = new JobInfo.Builder(JOB_ID, new ComponentName(this, DataSyncJobService.class));
+        jobBuilder.setMinimumLatency(getResources().getInteger(R.integer.example_delay_seconds) * 1000L);
+        jobBuilder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+
+        ////////////////////////////////////////////////
+        // Other options we have to configure our job //
+        ////////////////////////////////////////////////
+
+        //Require the device to be idle because we're going to use some resources and don't want to get in the way of the user
+        //This can delay your job significantly, so use it if the timing is not critical
+        //jobBuilder.setRequiresDeviceIdle(true);
+
+        //If we need to implement a retry-and-die strategy for this job, we no longer need to handle the retries and especially
+        //the increasing retry interval. We let the JobBuilder and the JobService framework handle all of that for us.
+        //NOTE: Using this method in conjunction with #setRequiresDeviceIdle will result in an exception.
+        //jobBuilder.setBackoffCriteria(
+        //        getResources().getInteger(R.integer.example_delay_seconds) * 1000L, JobInfo.BACKOFF_POLICY_EXPONENTIAL);
+
+        //If we want to ensure that our job is run by the time a MAXIMUM time has elapsed, regardless of other preconditions,
+        //then we can set this value
+        //jobBuilder.setOverrideDeadline(getResources().getInteger(R.integer.example_delay_seconds) * 1000L * 10);
+
+        //Set this job to run periodically on a given schedule. This method will result in an error if used in conjunction with
+        //setMinimumLatency or setOverrideDeadline
+        //jobBuilder.setPeriodic(1000L);
+
+        //We need this job to persist across device restarts. This method requires the RECEIVE_BOOT_COMPLETED permission, but
+        //will ensure that we do not lose the opportunity to do important work if the device runs out of battery
+        //jobBuilder.setPersisted(true);
+
+        //Pretty straight-forward
+        //jobBuilder.setRequiresCharging(true);
+
+        //Provide additional data to the service with a PersistableBundle. This is a new type of Bundle and is more limited in
+        //what it can hold in its values
+        //PersistableBundle extras = new PersistableBundle(2);
+        //extras.putInt("someKey", 42);
+        //extras.putString("someOtherKey", "A Value");
+        //jobBuilder.setExtras(extras);
+
+        ////////////////////////////////////////////////
+        // Other options we have to configure our job //
+        ////////////////////////////////////////////////
+
+        jobScheduler.schedule(jobBuilder.build());
+        finish();
     }
 }
---
+++ app/src/main/java/rowley/androidjobschedulerexample/scheduled/scheduler/DataSyncJobService.java
@@ -0,0 +1,94 @@
+package rowley.androidjobschedulerexample.scheduled.scheduler;
+
+import android.annotation.TargetApi;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import rowley.androidjobschedulerexample.R;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+/**
+ * This class demonstrates the ability to easily handle and retry long-running tasks using the JobScheduler. All of the setup
+ * work is done when the job is created in the MainActivity. This class's only concern is completing the work, or letting the
+ * system know that it was unable to complete the work. It keeps things focused, simple and clean. The only catch to this method
+ * is that, unlike extending an IntentService, we must manage our own thread work here. We simplify this by using RxJava.
+ */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class DataSyncJobService extends JobService {
+    private final String TAG = DataSyncJobService.class.getSimpleName();
+    public static final String SHARED_PREFS_KEY = "default";
+    public static final String FILES_COMPLETED_BUNDLE_KEY = "filesCompleted";
+    private final int TOTAL_FILES = 50;
+    private long INTERVAL_TIME = 100;
+    private TimeUnit INTERVAL_UNIT = TimeUnit.MILLISECONDS;
+
+    private volatile int completedFiles;
+    private volatile boolean hasBeenCanceled = false;
+    private Subscription subscription;
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        Toast.makeText(DataSyncJobService.this, R.string.starting_task_message, Toast.LENGTH_LONG).show();
+
+        //NOTE I tried using params.getExtras().getInt(...) to return either the default of 0 or a value that I saved to the
+        //bundle on a previous attempt, but it seems that any writes I make to the params.getExtras() bundle do not persist
+        //across attempts. So, if we got cut short in a previous attempt we do not know where we left off. We have two options,
+        //either just start again from the beginning, or find another way to manage half-done work. For the sake of simplicity
+        //I'm just using a shared pref to stash that value. Probably not the best way, but it's a way...
+        completedFiles = getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE).getInt(FILES_COMPLETED_BUNDLE_KEY, 0);
+
+        subscription = Observable.interval(INTERVAL_TIME, INTERVAL_UNIT).takeUntil(new Func1<Long, Boolean>() {
+            @Override
+            public Boolean call(Long aLong) {
+                return completedFiles++ == TOTAL_FILES || hasBeenCanceled;
+            }
+        }).subscribeOn(Schedulers.computation()).observeOn(Schedulers.immediate()).subscribe(new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(DataSyncJobService.this, R.string.finished_task_message, Toast.LENGTH_LONG).show();
+                //Let Android know that we are no longer working in the background, and that we do not need to reschedule the job
+                jobFinished(params, false);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                //Let Android know that we are no longer working in the background, but that we DO need to reschedule the job
+                jobFinished(params, true);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Log.d(TAG, String.format(getString(R.string.big_files_downloaded_formatted), completedFiles));
+            }
+        });
+
+        //If the completedFiles count is less than TOTAL_FILES then we have some work to do on a background thread. This method
+        //runs synchronously on the app's MainThread, so we want to move our work to the background and tell Android that we are
+        //continuing work even though this method has returned.
+        return completedFiles < TOTAL_FILES;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        hasBeenCanceled = true;
+        if(subscription != null) {
+            subscription.unsubscribe();
+        }
+        getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE).edit().putInt(FILES_COMPLETED_BUNDLE_KEY, completedFiles).apply();
+
+        //We were instructed to stop this job for some reason. Android wants to know whether we still have work to do, and it
+        //it should therefore reschedule the job according to our retry criteria and/or when the conditions are again met.
+        //Do we still have work to do?
+        return completedFiles < TOTAL_FILES;
+    }
+}
